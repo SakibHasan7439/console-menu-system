b8acf3d145c9727629389dcbd34d6372a67735a4
sakibHasan7439 <sakibhasanb120@gmail.com>
2025-04-26 21:45:50 +0600

    successfully showed the user history part

--- menu.cpp
+++ menu.cpp
@@ -3,35 +3,30 @@
 #include<vector>
 using namespace std;
 
-vector<string> showHistory(){
-    vector<string> history_items = {};
-    return history_items;
-}
+void showHistory(vector<string>& history_items){
+    cout<<"Your History is: ";
+    for(string item : history_items){
+        cout<<item<<endl;
+    };
+};
 
-void showMenu(int menuNumber, int num1, int num2){
+void showMenu(int menuNumber, int num1, int num2, vector<string>& history_items){
     int result;
-    vector<string> history = showHistory();
 
-    if(menuNumber == 5){
-        for(string item : history){
-            cout<<item<<endl;
-        }
-        return;
-        
-    }else if(menuNumber == 1){
+    if(menuNumber == 1){
         result = num1 + num2;
         cout<<num1<< " + "<<num2<< " = "<< result<<endl;
-        history.push_back(to_string(num1) + " + " + to_string(num2) + " = " + to_string(result));
+        history_items.push_back(to_string(num1) + " + " + to_string(num2) + " = " + to_string(result));
 
     }else if(menuNumber == 2){
         result = num1 - num2;
         cout<<num1<< " - "<<num2<< " = "<<result<<endl;
-        history.push_back(to_string(num1) + " - " + to_string(num2) + " = " + to_string(result));
+        history_items.push_back(to_string(num1) + " - " + to_string(num2) + " = " + to_string(result));
 
     }else if(menuNumber == 3){
         result = num1 * num2;
         cout<<num1<< " * "<<num2<< " = "<< result<<endl;
-        history.push_back(to_string(num1) + " * " + to_string(num2) + " = " + to_string(result));
+        history_items.push_back(to_string(num1) + " * " + to_string(num2) + " = " + to_string(result));
 
     }else {
         if(num2 == 0){
@@ -40,11 +35,12 @@ void showMenu(int menuNumber, int num1, int num2){
         }
         cout<<num1<< " / "<<num2<< " = "<< num1 / num2<<endl;
         result = num1 / num2;
-        history.push_back(to_string(num1) + " + " + to_string(num2) + " = " + to_string(result));
+        history_items.push_back(to_string(num1) + " + " + to_string(num2) + " = " + to_string(result));
     };
 };
 
 int main() {
+    vector<string> history_items = {};
     int num1, num2;
     int menuNumber;
     bool isExit = false;
@@ -58,22 +54,26 @@ int main() {
         string sixthItem = "6. Exit";
         cout<<firstItem<<endl<<secondItem<<endl<<thirdItem<<endl<<forthItem<<endl<<fifthItem<<endl<<sixthItem<<endl;
     
-        cout<<"Choose a menu item for Mathmatical calculation or enter 5 to exit: ";
+        cout<<"Choose a menu item for Mathmatical calculation or enter 6 to exit: ";
         cin>>menuNumber;
         if(menuNumber == 6){
-            cout<<"Thank you for using it. GoodBye!";
+            cout<<"Thank you for using it. GoodBye!"<<endl;
             break;
 
         }else if(menuNumber > 5 || menuNumber < 0){
             cout<<"invalid user input! Please try again.."<<endl;
             
+        }else if(menuNumber == 5){
+            showHistory(history_items);
+            cout<<endl;
+
         }else {
             cout<<"Enter first Number: ";
             cin>>num1;
         
             cout<<"Enter second Number: ";
             cin>>num2;
-            showMenu(menuNumber, num1, num2);
+            showMenu(menuNumber, num1, num2, history_items);
         };
     };
 }
